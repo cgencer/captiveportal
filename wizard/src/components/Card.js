77f4c6e5ea8d0c82fc37c403787c1dc7e71c9efc
Cem Gencer <cem.gencer@me.com>
2017-07-14 00:03:59 +0300

    ...

---
+++ wizard/src/components/Card.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Link} from 'react-router';
+import { RIEToggle, RIEInput, RIETextArea, RIENumber, RIETags, RIESelect } from 'riek';
+import _ from 'lodash';
+
+import '../styles/wizard.css';
+
+// Since this component is simple and static, there's no parent container for it.
+const Card = (props) => {
+  return (
+
+    <div className="card mx-auto rounded loginCard">
+      <div className="container-fluid row">
+
+        <div className="logoMobile">
+          <img className="card-img-top-mobile" src="../img/turkcell-logo.png" alt="Operator logo" />
+        </div>
+
+        <div className="col-sm-5 roundedCorners-leftSide" id="left-part">
+          {props.leftContent}
+        </div>
+
+        <div className="col-sm-7 roundedCorners-rightSide" id="right-part">
+
+          <div className="row">
+            <div className="col-11 offset-1">
+              <div className="logoDesktop">
+                <img className="" src="../img/turkcell-logo.png" alt="Operator logo" />
+              </div>
+            </div>
+          <div id="content">
+          {props.riteContent}
+          </div>
+
+          <div className="col-11 offset-1">&nbsp;</div>
+          <div className="col-11 offset-1 card-footer text-center">
+            <small className="text-muted">{props.footerContent}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Card;
