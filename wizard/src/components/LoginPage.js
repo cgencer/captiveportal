6c5aaacc82ecfe077f49dcb87905f6a36dc02446
Cem Gencer <cem.gencer@me.com>
2017-07-14 00:21:48 +0300

    wizard components init

--- wizard/src/components/LoginPage.js
+++ wizard/src/components/LoginPage.js
@@ -2,14 +2,15 @@ import React from 'react';
 import {Link} from 'react-router';
 import { RIEToggle, RIEInput, RIETextArea, RIENumber, RIETags, RIESelect } from 'riek';
 import _ from 'lodash';
+import Card from './Card';
 
 import '../styles/wizard.css';
 
 // Since this component is simple and static, there's no parent container for it.
-const WizardPage = () => {
+const LoginPage = () => {
   return (
-    <Card leftContent="test area" riteContent="" footerContent="" />
+    <Card leftContent="test area" riteContent="this is the LOGIN page" footerContent="Powered by Turkcell" />
   );
 };
 
-export default WizardPage;
+export default LoginPage;
--- wizard/src/components/LoginSPage.js
+++ wizard/src/components/LoginSPage.js
@@ -2,44 +2,15 @@ import React from 'react';
 import {Link} from 'react-router';
 import { RIEToggle, RIEInput, RIETextArea, RIENumber, RIETags, RIESelect } from 'riek';
 import _ from 'lodash';
+import Card from './Card';
 
 import '../styles/wizard.css';
 
 // Since this component is simple and static, there's no parent container for it.
-const WizardPage = () => {
+const LoginSPage = () => {
   return (
-
-    <div className="card mx-auto rounded loginCard">
-      <div className="container-fluid row">
-
-        <div className="logoMobile">
-          <img className="card-img-top-mobile" src="../img/turkcell-logo.png" alt="Operator logo" />
-        </div>
-
-        <div className="col-sm-5 roundedCorners-leftSide" id="left-part">
-          lefts
-        </div>
-
-        <div className="col-sm-7 roundedCorners-rightSide" id="right-part">
-
-          <div className="row">
-            <div className="col-11 offset-1">
-              <div className="logoDesktop">
-
-                <img className="" src="../img/turkcell-logo.png" alt="Operator logo" />
-              </div>
-            </div>
-          <div id="content"></div>
-
-          <div className="col-11 offset-1">&nbsp;</div>
-          <div className="col-11 offset-1 card-footer text-center">
-            <small className="text-muted">Powered by Turkcell</small>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
+    <Card leftContent="test area" riteContent="this is the SHORT LOGIN page" footerContent="Powered by Turkcell" />
   );
 };
 
-export default WizardPage;
+export default LoginSPage;
